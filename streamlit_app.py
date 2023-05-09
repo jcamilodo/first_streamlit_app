aacebfbba9a5edd31abcd79765c352952b2ee875
Juan Camilo Díaz Ortega <jcamilodo@users.noreply.github.com>
2023-05-09 17:01:29 -0500

    Update streamlit_app.py
    Add request integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import requests
+
 
 streamlit.title("My parents New Healthy Diner")
 
@@ -20,6 +22,10 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
